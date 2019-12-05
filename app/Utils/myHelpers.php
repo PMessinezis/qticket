c3f17377da3a7e0bcbe210273f78fcd5e818bb36
Redoc Tsuj <Redoc.Tsuj@gmail.com>
2019-12-05 13:37:24 +0000

    config generic

--- app/Utils/myHelpers.php
+++ app/Utils/myHelpers.php
@@ -1,6 +1,7 @@
 <?php
 
-function getLDAPDomain() {
+function getLDAPDomain()
+{
 	$domain = getCurrentUserDomain();
 	if ($domain == '') {
 		$domain = $_SERVER["USERDOMAIN"];
@@ -8,7 +9,8 @@ function getLDAPDomain() {
 	return strtoupper($domain);
 }
 
-function ldapcheckfixdates($v) {
+function ldapcheckfixdates($v)
+{
 	if (is_numeric($v) and strlen($v) > 13) {
 		$win_secs = (int) ($v / 10000000);
 		$unix_timestamp = (int) ($win_secs - 11644473600);
@@ -17,7 +19,8 @@ function ldapcheckfixdates($v) {
 	return $v;
 }
 
-function array_map_assoc($array) {
+function array_map_assoc($array)
+{
 	$r = array();
 	foreach ($array as $key => $value) {
 		$r[$key] = $value;
@@ -26,7 +29,8 @@ function array_map_assoc($array) {
 	return $r;
 }
 
-function ldapQuery($ldap_base, $ldap_filter, $ldap_fields) {
+function ldapQuery($ldap_base, $ldap_filter, $ldap_fields)
+{
 	$ldapinfoR = [];
 	$domain = getLDAPDomain();
 	if (function_exists('ldap_connect')) {
@@ -54,7 +58,6 @@ function ldapQuery($ldap_base, $ldap_filter, $ldap_fields) {
 					}
 					$rec['dn'] = $r['dn'];
 					$ldapinfoR[] = $rec;
-
 				} else {
 					$attrV = $r[$ldap_fields[0]];
 					$ldapinfoR[] = is_array($attrV) ? $attrV[0] : $attrV;
@@ -65,19 +68,22 @@ function ldapQuery($ldap_base, $ldap_filter, $ldap_fields) {
 	return $ldapinfoR;
 }
 
-function getQQSecGroups() {
+function getQQSecGroups()
+{
 	$domain = getLDAPDomain();
 	return ldapQuery(config('qticket.LDAP_ROOT_' . $domain), "(&(objectCategory=group)(samaccountname=sec_dpt_*))", ["samaccountname", "dn"]);
 }
 
-function getQQUsers() {
-	$base = 'OU=QQuant Users,DC=qualco,DC=int';
+function getQQUsers()
+{
+	$base = 'OU=xxxxxx Users,DC=xxxxxx,DC=int';
 	$filter = "(&(objectCategory=person)(objectClass=user)(samaccountname=*))";
 	$fields = array('samaccountname');
 	return ldapQuery($base, $filter, $fields);
 }
 
-function checkUser($u) {
+function checkUser($u)
+{
 	$sn = $u['sn'] ?? '';
 	$dn = $u['dn'];
 	$m = $u['mail'] ?? '';
@@ -88,13 +94,13 @@ function checkUser($u) {
 	$QQ = false;
 	$stat = "NOT_QQ";
 	if (!($flags & 2)) {
-		$QQ = $QQ || stripos($m, 'qquant.gr') !== false;
-		$QQ = $QQ || stripos($sn, 'qquant') !== false;
-		$QQ = $QQ || stripos($cn, 'qquant') !== false;
-		$QQ = $QQ || stripos($desc, 'qquant') !== false;
-		$QQ = $QQ || stripos($uid, 'qquant') !== false;
+		$QQ = $QQ || stripos($m, 'xxxxxx.gr') !== false;
+		$QQ = $QQ || stripos($sn, 'xxxxxx') !== false;
+		$QQ = $QQ || stripos($cn, 'xxxxxx') !== false;
+		$QQ = $QQ || stripos($desc, 'xxxxxx') !== false;
+		$QQ = $QQ || stripos($uid, 'xxxxxx') !== false;
 		if ($QQ) {
-			$stat = "QQUANT";
+			$stat = "xxxxxx";
 		}
 	} else {
 		$stat = "DISABLED";
@@ -103,8 +109,9 @@ function checkUser($u) {
 	mylog("$stat;$uid;$sn;$dn;$desc;$m");
 }
 
-function checkForQQUsers() {
-	$base = 'DC=qualco,DC=int';
+function checkForQQUsers()
+{
+	$base = 'DC=xxxxxx,DC=int';
 	$accounts = [];
 	foreach (range('a', 'z') as $L) {
 		$filter = "(&(objectCategory=person)(objectClass=user)(samaccountname=$L*))";
@@ -116,9 +123,10 @@ function checkForQQUsers() {
 	}
 }
 
-function findUsersOutsideQQou() {
+function findUsersOutsideQQou()
+{
 	$users = App\User::all();
-	$base = 'DC=qualco,DC=int';
+	$base = 'DC=xxxxxx,DC=int';
 	$fields = array('samaccountname', 'sn', 'mail', 'useraccountcontrol');
 	foreach ($users as $u) {
 		$uid = $u->uid;
@@ -126,14 +134,15 @@ function findUsersOutsideQQou() {
 		$adu = ldapQuery($base, $filter, $fields);
 		if (isset($adu[0])) {
 			$ou = $adu[0]['dn'];
-			if (!stripos($ou, 'qquant')) {
+			if (!stripos($ou, 'xxxxxx')) {
 				echo $uid . ' in ' . $ou . PHP_EOL;
 			}
 		}
 	}
 }
 
-function addRefreshQQUsers() {
+function addRefreshQQUsers()
+{
 	$QQ = getQQUsers();
 	foreach ($QQ as $uid) {
 		echo $uid . PHP_EOL;
@@ -141,7 +150,8 @@ function addRefreshQQUsers() {
 	}
 }
 
-function ldapinfoByAttr($attrN, $attrV) {
+function ldapinfoByAttr($attrN, $attrV)
+{
 	$ldapinfo = collect([]);
 	if (function_exists('ldap_connect')) {
 		$domain = getCurrentUserDomain();
@@ -190,16 +200,20 @@ function ldapinfoByAttr($attrN, $attrV) {
 					}
 				}
 			}
-		} else { return ldap_error($ldap);}
+		} else {
+			return ldap_error($ldap);
+		}
 	}
 	return $ldapinfo;
 }
 
-function ldapinfo($uid) {
+function ldapinfo($uid)
+{
 	return ldapinfoByAttr('sAMAccountName', $uid);
 }
 
-function getADGroupMembers($gid) {
+function getADGroupMembers($gid)
+{
 	$members = [];
 	$domain = getLDAPDomain();
 	$base = config('qticket.LDAP_ROOT_' . $domain);
@@ -215,7 +229,8 @@ function getADGroupMembers($gid) {
 	return $members;
 }
 
-function getADGroupMembersViaDN($gdn) {
+function getADGroupMembersViaDN($gdn)
+{
 	$domain = getLDAPDomain();
 	$base = config('qticket.LDAP_ROOT_' . $domain);
 	$filter = "(&(objectCategory=person)(memberof=$gdn))";
@@ -223,7 +238,8 @@ function getADGroupMembersViaDN($gdn) {
 	return ldapQuery($base, $filter, $fields);
 }
 
-function addSecGroups() {
+function addSecGroups()
+{
 	$adgroups = getQQSecGroups();
 	foreach ($adgroups as $g) {
 		$gid = $g["samaccountname"];
@@ -239,7 +255,8 @@ function addSecGroups() {
 	}
 }
 
-function addSecGroupsMembers() {
+function addSecGroupsMembers()
+{
 	$adgroups = App\ADGroup::all();
 	foreach ($adgroups as $adg) {
 		$gid = $adg->gid;
@@ -249,7 +266,8 @@ function addSecGroupsMembers() {
 	}
 }
 
-function refreshAllUsersFromLDAP() {
+function refreshAllUsersFromLDAP()
+{
 	$users = App\User::all();
 	foreach ($users as $u) {
 		$u->refreshFromLDAP();
@@ -258,7 +276,8 @@ function refreshAllUsersFromLDAP() {
 	addSecGroupsMembers();
 }
 
-function myURL($s) {
+function myURL($s)
+{
 	$root = isset($_SERVER['HTTP_REALROOT']) ? $_SERVER['HTTP_REALROOT'] : '';
 	// echo $s, $_SERVER['HTTP_REALROOT'];
 	if ($root) {
@@ -270,7 +289,8 @@ function myURL($s) {
 	}
 }
 
-function groupMembersEmails($group_id) {
+function groupMembersEmails($group_id)
+{
 	$g = \App\Group::where('id', $group_id)->first();
 	$e = [];
 	if ($g) {
@@ -283,7 +303,8 @@ function groupMembersEmails($group_id) {
 	return $e;
 }
 
-function qticketReviewersEmails() {
+function qticketReviewersEmails()
+{
 	$g = \App\Group::where('name', config('qticket.reviewers'))->first();
 	if ($g) {
 		return groupMembersEmails($g->id);
@@ -292,7 +313,8 @@ function qticketReviewersEmails() {
 	}
 }
 
-function curl_download($Url) {
+function curl_download($Url)
+{
 
 	// is cURL installed yet?
 	if (!function_exists('curl_init')) {
@@ -331,7 +353,8 @@ function curl_download($Url) {
 	return $output;
 }
 
-function newTicket($title, $description, $isCritical) {
+function newTicket($title, $description, $isCritical)
+{
 	$id = 0;
 	$user = \App\User::where('uid', 'qticket')->first();
 	Auth::login($user, true);
@@ -356,10 +379,10 @@ function newTicket($title, $description, $isCritical) {
 	$id = $t->id;
 	$t->addUpdate('Ticket Opened', null, null, true); // this will now also take care of the TicketOpened email
 	return $id;
-
 }
 
-function readCRSensors() {
+function readCRSensors()
+{
 	$RET = "";
 	$xmlstr = curl_download('http://guest:P%40ssw0rd%21@10.102.8.73/data.xml');
 	if ($xmlstr > "") {
@@ -445,29 +468,31 @@ function readCRSensors() {
 	return $RET;
 }
 
-function getGuides() {
+function getGuides()
+{
 	return [
-		['id' => 'emailonphone', 'file' => 'Mail to phone.pdf', 'label' => 'Setup QQuant email on the phone'],
+		['id' => 'emailonphone', 'file' => 'Mail to phone.pdf', 'label' => 'Setup xxxxxx email on the phone'],
 		['id' => 'webmail', 'file' => 'Web Mail Access.pdf', 'label' => 'Open your email via webmail'],
 		['id' => 'webmailpass', 'file' => 'Reset AD User Password.pdf', 'label' => 'Change your password via webmail'],
 		['id' => 'credmgr', 'file' => 'To open Credentials Manager.pdf', 'label' => 'Add Credentials Manager Entry'],
 		['id' => 'wifi', 'file' => 'Wi-fi Manual.pdf', 'label' => 'Connect your phone to the corporate WiFi'],
-		['id' => 'emailonIphone', 'file' => 'Email - iOS Devices.pdf', 'label' => 'Setup QQuant email on the Iphone'],
+		['id' => 'emailonIphone', 'file' => 'Email - iOS Devices.pdf', 'label' => 'Setup xxxxxx email on the Iphone'],
 
 	];
 }
 
-function getHowToGuides() {
+function getHowToGuides()
+{
 	$g = App\Filelink::all();
 	$hgt = $g->map(function ($item, $key) {
 		return ['id' => $item->id, 'link' => $item->link, 'label' => $item->description];
 	});
 
 	return $hgt->toArray();
-
 }
 
-function getStaticData() {
+function getStaticData()
+{
 	$u = Auth::User() ? Auth::User()->load('resolver') : null;
 	$res = collect([
 
@@ -482,22 +507,24 @@ function getStaticData() {
 		'users' => App\User::orderBy('lastname')->get(),
 		'guides' => getGuides(),
 		'howtoguides' => getHowToGuides(),
-	])
-	;
+	]);
 	// dd($res);
 	return $res;
 }
 
-function mylog($s, $symbols = []) {
+function mylog($s, $symbols = [])
+{
 	$me = Auth::User();
 	$myInfo = $me ? 'User: ' . $me->uid . ' | ' : '';
 	Log::info($myInfo . $s, $symbols);
 }
 
-function setCurrentUserDomain($adomain) {
+function setCurrentUserDomain($adomain)
+{
 	\Config::set('qticket.LDAP_USER_DOMAIN', $adomain);
 }
 
-function getCurrentUserDomain() {
+function getCurrentUserDomain()
+{
 	return strtoupper(config('qticket.LDAP_USER_DOMAIN'));
 }
